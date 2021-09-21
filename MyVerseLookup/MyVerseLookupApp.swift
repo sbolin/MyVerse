03b385d682ee2e25bb7f245d9b89ef3302c88315
Scott Bolin <sbolin@gmail.com>
2021-09-21 18:08:56 +0700

    Initial Commit

---
+++ MyVerseLookup/MyVerseLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVerseLookupApp.swift
+//  MyVerseLookup
+//
+//  Created by Scott Bolin on 21-Sep-21.
+//
+
+import SwiftUI
+
+@main
+struct MyVerseLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
